04709f80077cf557056a564d6030dc67f8eabea1
Mirjam <fmirka.fm@gmail.com>
2020-12-04 11:46:50 +0100

    Practice 2

---
+++ week-02/day-2/practice2.ts
@@ -0,0 +1,36 @@
+//Exercise 1
+let myName = "Mirjam"
+let myAge = 25
+let myHeight = 1.75
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+
+//Exercise 2
+let num1 = 22
+let num2 = 13
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+//Exercise 3
+let dailyHours = 6
+let semesterDuration = 17
+let weeklyHours = dailyHours * 7
+let workdayWeeklyHours = dailyHours * 5
+let averageWeeklyHours = 52
+let totalSemesterHours = weeklyHours * semesterDuration 
+let totalSemesterHoursPercentage = totalSemesterHours / 100
+
+console.log(weeklyHours * semesterDuration);
+console.log(workdayWeeklyHours *semesterDuration);
+console.log(averageWeeklyHours / totalSemesterHoursPercentage);
+console.log(averageWeeklyHours / totalSemesterHours * 100);
+
+
+
+
