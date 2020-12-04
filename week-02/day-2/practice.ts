a6d06bd0b691924e3a88b7dc5c39ba411d27dc6f
Mirjam <fmirka.fm@gmail.com>
2020-12-04 11:00:15 +0100

    Practice 1

---
+++ week-02/day-2/practice.ts
@@ -0,0 +1,136 @@
+console.log ("Hello World");
+
+//Exercise 2
+console.log("Hello Mirjam");
+
+
+//Exercise 3
+var firstLine = "All the king's horses and all the king's men"
+var secondLine ="Humpty Dumpty had a great fall."
+var thirdLine = "Humpty Dumpty sat on a wall,"
+var fourthLine = "Could't put Humpty together again."
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
+console.log(fourthLine);
+
+//Exercise 4
+var classMateE = "Hello, Esther"
+var classMateM = "Hello, Mary"
+var classMateJ = "Hello, Joe"
+
+console.log(classMateE);
+console.log(classMateM);
+console.log(classMateJ);
+
+//Exercise 5
+//Prints a sting to the terminal
+var welcome = "Hello World!"
+
+console.log(welcome);
+
+//Prints an integer to the terminal
+var integer = 42
+
+console.log(integer);
+
+//Prints a floating point number to the terminal window
+var floatingPoint = 3.141592
+
+console.log(floatingPoint);
+
+//Prints a boolean to the terminal window
+
+console.log(true);
+
+//Positive, negative and zero integers
+
+var positiveInteger = 42
+var negativeInteger = -1
+var zero = 0
+
+console.log(positiveInteger);
+console.log(negativeInteger);
+console.log(zero);
+
+//Rounded numbers
+var roundedNum = 12341234123412341234
+
+console.log(roundedNum);
+
+//Floats
+var floatPositive = 3.1415
+var floatNegative = -1.5
+var floatZero = 0.0
+
+console.log(floatPositive);
+console.log(floatNegative);
+console.log(floatZero);
+
+//In floats, leading and closing zero is not mandatory
+
+var leadingZero = .5
+var closingZero = 5. 
+
+console.log(leadingZero);
+console.log(closingZero);
+
+//Arithmetic operations
+var sum = 1 + 1
+var deduction = 1 - 1 
+var multiply = 3 * 4
+var divide = 6 / 2
+var divide2 = 5 / 2
+var percentage = 5 % 2
+
+console.log(sum);
+console.log(deduction);
+console.log(multiply);
+console.log(divide);
+console.log(divide2);
+console.log(percentage);
+
+//Booleans
+//True, False
+
+console.log(true);
+console.log(false);
+
+//Negation
+
+console.log(!true);
+console.log(!false);
+
+//And
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+//Or
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//String and special characters
+console.log("apple");
+console.log("don\'t");
+console.log('"Everything you can imagine is real."-Picasso');
+
+//String Operators
+//Concatenation
+console.log("tooth" + "brush");
+
+
+//Concat string with number
+console.log("My favourite number is: " + 8);
+
+
+
+
+
+
+
+
