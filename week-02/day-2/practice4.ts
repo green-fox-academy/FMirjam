ae79e6955f05593ec0b509ec4279303b2d99dc37
Mirjam <fmirka.fm@gmail.com>
2020-12-05 07:42:34 +0100

    Practice 4

---
+++ week-02/day-2/practice4.ts
@@ -0,0 +1,61 @@
+//Nothings
+undefined;
+null;
+NaN;
+
+null !== undefined;
+NaN !== undefined;
+null !== NaN;
+
+null == undefined;
+
+typeof undefined;
+typeof null;
+typeof NaN;
+
+let apple;
+console.log(apple);
+console.log(apple + 1);
+apple = null;
+console.log(apple);
+
+//Conditionals
+let a: number = 13;
+
+if (a === 13) {
+  console.log('Yaaay! The value of the \'a\' variable is 13'); // This block will run
+}
+
+if (a === 8) {
+  console.log('Yaaay! The value of the \'a\' variable is 8'); // This block will NOT run
+}
+
+
+let b: number = 20;
+
+if (b < 10) {
+  console.log('Yaaay! The value of the \'b\' variable is lower than 10');  // This block will NOT run
+} else {
+  console.log('Yaaay! The value of the \'b\' variable is higher than 10'); // This block will run
+}
+
+let c: number = 15;
+
+if (c < 10) {
+  console.log('Yaaay! The value of the \'c\' variable is lower than 10');  // This block will NOT run
+} else if (c < 20) {
+  console.log('Yaaay! The value of the \'c\' variable is higher than 10'); // This block will run
+} else {
+  console.log('Yaaay! The value of the \'c\' variable is higher than 10'); // This block will NOT run
+}
+
+let thirsty: boolean = true;
+let hungry: boolean = false;
+
+if (thirsty && hungry) {
+  console.log('Lunch time!');
+} else if (thirsty || hungry) {
+  console.log('Snack time!');
+} else {
+  console.log('No food for you.');
+}
