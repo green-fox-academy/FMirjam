c18eb2cab0ac7b6513645c35b8a2446ba2473df1
Mirjam <fmirka.fm@gmail.com>
2020-12-13 08:15:42 +0100

    working folders and documents are created

---
+++ week-04/day-1/exercise_9/centeredboxes.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws one square and takes 1 parameters:
+// The square size
+// and draws a square of that size to the center of the canvas.
+// Draw 3 squares with that function.
+// Avoid code duplication.
