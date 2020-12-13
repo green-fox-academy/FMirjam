e92aeefccf33a477f4e6f09420e8c851f7a17e42
Mirjam <fmirka.fm@gmail.com>
2020-12-13 07:40:06 +0100

    Task description added, redo them for further practice

---
+++ week-03/day-1/colors.ts
@@ -0,0 +1,15 @@
+'use strict';
+// -  Create a two dimensional list
+//    which can contain the different shades of specified colors
+// -  In `colors[0]` store the shades of green:
+//    `"lime", "forest green", "olive", "pale green", "spring green"`
+// -  In `colors[1]` store the shades of red:
+//    `"orange red", "red", "tomato"`
+// -  In `colors[2]` store the shades of pink:
+//    `"orchid", "violet", "pink", "hot pink"`
+
+let colours = [];
+
+console.log(colours[0]);
+console.log(colours[1]);
+console.log(colours[2]);
---
+++ week-03/day-1/greeterfunction.ts
@@ -0,0 +1,4 @@
+// -  Create variable named `nameToGreet` and assign the value `Green Fox` to it
+// -  Create a function called `greet()` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Green Fox`
+// -  Greet `nameToGreet`
