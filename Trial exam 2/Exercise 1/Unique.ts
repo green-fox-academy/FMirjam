5ee45bbe71e085f0243d54639cb068fe724cb4d9
Mirjam <fmirka.fm@gmail.com>
2021-02-23 18:06:04 +0100

    unique changes

--- Trial exam 2/Exercise 1/Unique.ts
+++ Trial exam 2/Exercise 1/Unique.ts
@@ -2,27 +2,30 @@
 // Create a function that takes a string as parameter
 //and returns a list with the unique letters of the given string.
 
+import { count } from 'console';
+
 function Unique(word: string): string[] {
-  word.toLowerCase().split('');
+  const splittedWord = word.toLowerCase().split('');
 
   let counter: Object = {};
 
-  for (let character of word) {
+  for (let character of splittedWord) {
     if (counter[character]) {
       counter[character]++;
     } else {
       counter[character] = 1;
     }
   }
+  return Object.keys(counter);
 
-  let uniqueArray: string[] = [];
+  // let uniqueArray: string[] = [];
 
-  for (let i: number = 0; i < Object.keys(counter).length; i++) {
-    if (Object.values(counter)[i] === 1) {
-      uniqueArray.push(Object.keys(counter)[i]);
-    }
-  }
-  return uniqueArray;
+  // for (let i: number = 0; i < Object.keys(counter).length; i++) {
+  //   if (Object.values(counter)[i] > Object.values(counter)[i + 1]) {
+  //     uniqueArray.push(Object.keys(counter)[i]);
+  //   }
+  // }
+  // return uniqueArray;
 }
 
-console.log(Unique('anagram'));
+console.log(Unique('lamafaalll'));
