38968a6931b96a357481f95d810d3365478a11c0
Mirjam <fmirka.fm@gmail.com>
2021-03-02 17:54:18 +0100

    JS started

---
+++ week-10/Gallery project/main.js
@@ -0,0 +1,4 @@
+const previous = document.querySelector('#previous');
+const showedpicutre = document.querySelector('.showedpicture');
+const next = document.querySelector('#next');
+const thumbnailImages = document.querySelectorAll('._photo');
