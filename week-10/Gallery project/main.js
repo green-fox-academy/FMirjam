254cf636107c5246a5ebd608c9d818a64384be20
Mirjam <fmirka.fm@gmail.com>
2021-03-03 09:02:05 +0100

    slideshow works

--- week-10/Gallery project/main.js
+++ week-10/Gallery project/main.js
@@ -1,4 +1,17 @@
-const previous = document.querySelector('#previous');
-const showedpicutre = document.querySelector('.showedpicture');
-const next = document.querySelector('#next');
-const thumbnailImages = document.querySelectorAll('._photo');
+const imageIndex = [1,1];
+showImages(1,0);
+
+function plusImage(n, no) {
+  showImages(imageIndex[no] += n, no);
+}
+
+function showImages(n, no) {
+  let i;
+  let x = document.getElementsByClassName("showedpicture");
+  if (n > x.length) {imageIndex[no] = 1} 
+  if (n < 1) {imageIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  x[imageIndex[no]-1].style.display = "block"; 
+}
