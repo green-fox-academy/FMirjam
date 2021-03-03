193ab8bc803de0c869f3e2ef5c5b219c8b9d79af
Mirjam <fmirka.fm@gmail.com>
2021-03-03 16:42:29 +0100

    delete smallImage related code

--- week-10/Gallery project/main.js
+++ week-10/Gallery project/main.js
@@ -1,17 +1,25 @@
-const imageIndex = [1,1];
-showImages(1,0);
+const imageIndex = [1, 0];
+showImages(1, 0);
 
 function plusImage(n, no) {
-  showImages(imageIndex[no] += n, no);
+  showImages((imageIndex[no] += n), no);
 }
+
 
 function showImages(n, no) {
   let i;
-  let x = document.getElementsByClassName("showedpicture");
-  if (n > x.length) {imageIndex[no] = 1} 
-  if (n < 1) {imageIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
+  let x = document.getElementsByClassName('showedpicture');
+  let smallImages = document.getElementsByClassName('._photo');
+  if (n > x.length) {
+    imageIndex[no] = 1;
   }
-  x[imageIndex[no]-1].style.display = "block"; 
+  if (n < 1) {
+    imageIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[imageIndex[no] - 1].style.display = 'block';
+  
 }
+
