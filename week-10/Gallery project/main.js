2f3d32f647f6f339ea473cb8beeffeb9dda55a0e
Mirjam <fmirka.fm@gmail.com>
2021-03-06 17:38:02 +0100

    Gallery project almost done

--- week-10/Gallery project/main.js
+++ week-10/Gallery project/main.js
@@ -2,6 +2,43 @@ const images = [1, 2, 3, 4, 5, 6, 7, 8];
 let currentImage = 1;
 let showedPicture = document.querySelector('.photo');
 let thumbnailImages = document.querySelectorAll('._photo');
+let photoHeader = document.querySelector('h2');
+let photoSubText = document.querySelector('p');
+let pictureDetails = {
+  1: {
+    header: 'New York',
+    subtext: 'Times Square',
+  },
+  2: {
+    header: 'New York',
+    subtext: 'Grand Central Station',
+  },
+  3: {
+    header: 'New York',
+    subtext: 'Empire State Building',
+  },
+  4: {
+    header: 'Cuba',
+    subtext: 'Oldtimer',
+  },
+  5: {
+    header: 'Cuba',
+    subtext: 'Colorful buildings',
+  },
+  6: {
+    header: 'Cuba',
+    subtext: 'José Martí Memorial',
+  },
+  7: {
+    header: 'Cuba',
+    subtext: 'Matanzas',
+  },
+  8: {
+    header: 'Cuba',
+    subtext: 'Sunset',
+  },
+};
+
 
 function previousImage() {
   if (currentImage === 1) {
@@ -27,6 +64,7 @@ function showImage(pictureNumber) {
   }
   showedPicture.src = `/img/${currentImage}.JPG`;
   handleActiveClassImage();
+  changeText();
 }
 
 function handleActiveClassImage() {
@@ -38,7 +76,9 @@ function handleActiveClassImage() {
   thumbnailImage.classList.add('active');
 }
 
-let pictureDetails = {1: {
-  header: 'Text',
-  subtext: 'Subtext'
-}}
+function changeText(){
+let currentHeader = pictureDetails[currentImage]['header'];
+let currentSubtext = pictureDetails[currentImage]['subtext'];
+photoHeader.innerHTML = currentHeader;
+photoSubText.innerHTML = currentSubtext;
+}
