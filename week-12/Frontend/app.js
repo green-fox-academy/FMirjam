f0329f1591dafaa88b55abfde750f23dadb4fe66
Mirjam <fmirka.fm@gmail.com>
2021-03-09 19:16:01 +0100

    append a done, do until does not work

--- week-12/Frontend/app.js
+++ week-12/Frontend/app.js
@@ -4,7 +4,9 @@ const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
 
+
 app.use(express.static('assets'));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
@@ -15,55 +17,94 @@ app.get('/', (req, res) => {
 });
 
 app.get('/doubling', (req, res) => {
- let input = req.query.input;
- let result;
- if (input !== undefined){
-     result = {
-         'recieved': input,
-         'result': input * 2,
-     }
- } else { 
-     result = {
-     'error': 'Please provide an input!'}
- }
+  let input = req.query.input;
+  let result;
+  if (input !== undefined) {
+    result = {
+      'recieved': input,
+      'result': input * 2,
+    };
+  } else {
+    result = {
+      'error': 'Please provide an input!',
+    };
+  }
   res.send(result);
 });
 
 app.get('/greeter', (req, res) => {
-    let name = req.query.name;
-    let title = req.query.title;
-    let result;
-    if(name !== undefined && title !== undefined){
-        result = {
-            'welcome_message': `Oh, hi there ${name}, my dear ${title}!`
-          }
-    } else if (name === undefined && title === undefined){
-        result = {
-            'error': `Please provide ${name} and ${title}!`
-        }
-    } else if (name !== undefined && title === undefined){
-        result = {
-            'error': `Please provide title!`
-        }
-    } else {
-        result = {
-            'error': `Please provide name!`
-        }
+  let name = req.query.name;
+  let title = req.query.title;
+  let result;
+  if (name !== undefined && title !== undefined) {
+    result = {
+      'welcome_message': `Oh, hi there ${name}, my dear ${title}!`,
+    };
+  } else if (name === undefined && title === undefined) {
+    result = {
+      'error': `Please provide ${name} and ${title}!`,
+    };
+  } else if (name !== undefined && title === undefined) {
+    result = {
+      'error': `Please provide title!`,
+    };
+  } else {
+    result = {
+      'error': `Please provide name!`,
+    };
+  }
+  res.send(result);
+});
+
+app.get('/appenda/:text', (req, res) => {
+  let text = req.params.text;
+  let result;
+  if (text !== undefined) {
+    result = {
+      'appended': text + 'a',
+    };
+  } else {
+    result = {
+      'error': 'Please provide an input!',
+    };
+  }
+  res.send(result);
+});
+
+app.post('/dountil/:action', (req, res) => {
+  let until = req.body.until;
+  let action = req.params.action;
+  let result;
+  if (action === 'sum') {
+    result = {
+        'until': until,
+        'result': sum(until),
     }
+  } else  if (action === 'factorial') {
+    result = {
+        'until': until,
+        'result': factorial(until),
+    }
+  } else {
+      result = {
+    'error': 'Please provide a number!',
+    }
+  }
     res.send(result);
 });
 
-app.get('/appenda/', (req, res) =>{
-    let text = req.params.text;
-    let result;
-    if (text !== undefined) {
-        result = {
-            'appended': text + 'a'
-        }
-    } else {
-        result = {
-            'error': 'Please provide an input!'
-        }
-    }
-    res.send(result)
-})
+function sum(number) {
+  let sum = 0;
+  for (let i = 0; i <= number; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function factorial(number) {
+  let factorial = 1;
+  for (let i = 2; i <= number; i++) {
+    factorial += i;
+  }
+  return factorial;
+}
