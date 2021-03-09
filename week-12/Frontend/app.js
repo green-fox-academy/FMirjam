bdfb46e0c72b381218945e3aa45e6d1cc527c9b6
Mirjam <fmirka.fm@gmail.com>
2021-03-09 17:08:26 +0100

    greeter is done

--- week-12/Frontend/app.js
+++ week-12/Frontend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.static('assets'));
 
@@ -11,4 +12,35 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/doubling', (req, res) => {
+ let input = req.query.input;
+ let result;
+ 
+  res.send(result);
+});
+
+app.get('/greeter', (req, res) => {
+    let name = req.query.name;
+    let title = req.query.title;
+    let result;
+    if(name !== undefined && title !== undefined){
+        result = {
+            'welcome_message': `Oh, hi there ${name}, my dear ${title}!`
+          }
+    } else if (name === undefined && title === undefined){
+        result = {
+            'error': `Please provide ${name} and ${title}!`
+        }
+    } else if (name !== undefined && title === undefined){
+        result = {
+            'error': `Please provide title!`
+        }
+    } else {
+        result = {
+            'error': `Please provide name!`
+        }
+    }
+    res.send(result);
 });
