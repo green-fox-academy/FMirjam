e537130f4cee41bef0bbe48125805d1c776128e6
Mirjam <fmirka.fm@gmail.com>
2021-03-13 15:29:50 +0100

    interval is missing

---
+++ week-12/day 3/Callback/Bomb defuse/bomb_defuse.js
@@ -0,0 +1,19 @@
+// Create a timeout that will set the display to "Bomb exploded" in 10 seconds
+// If you click on the button set the display to "Bomb defused" and stop the timeout
+// Extra: create an interval which will update the remaining seconds in the display
+
+const timeOut = setTimeout(() => {
+  alert('Bomb exploaded');
+}, 10000);
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  alert('Bomb defused');
+  clearTimeout(timeOut);
+});
+
+const interval = setInterval(updateTime, 1000);
+
+function updateTime() {
+  const display = document.querySelector('.display');
+}
