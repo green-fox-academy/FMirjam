cf476de6a750cbca9ee9e066d9b5242f6da82381
Mirjam <fmirka.fm@gmail.com>
2021-03-20 17:55:29 +0100

    bookstore done

--- week-13/day 1-2/Bookstore/public/script.js
+++ week-13/day 1-2/Bookstore/public/script.js
@@ -1,15 +1,20 @@
-const url = 'http://localhost:3000/fulldata';
 const tableBody = document.querySelector('tbody');
 const button = document.querySelector('button');
+const category = document.getElementById('category');
+const publisher = document.getElementById('publisher');
+const plt = document.getElementById('plt');
+const pgt = document.getElementById('pgt');
 
 button.addEventListener('click', () => {
-
-    getFullData();
-})
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+  getFullData();
+});
 
 function getFullData() {
   let http = new XMLHttpRequest();
-  http.open('GET', url, true);
+  http.open('GET', filterUrl(), true);
   http.onload = () => {
     let data = JSON.parse(http.responseText);
     for (let i = 0; i < data.length; i++) {
@@ -34,6 +39,44 @@ function getFullData() {
     }
   };
   http.send();
+}
+
+function filterUrl() {
+  let categoryValue = category.value;
+  let publisherValue = publisher.value;
+  let pltValue = plt.value;
+  let pgtValue = pgt.value;
+  let isFirstSearchField = true;
+  let url = 'http://localhost:3000/fulldata';
+  if (categoryValue) {
+    url = url + `?category=${categoryValue}`;
+    isFirstSearchField = false;
+  }
+  if (publisherValue) {
+    if (!isFirstSearchField) {
+      url = url + `&publisher=${publisherValue}`;
+    } else {
+      url = url + `?publisher=${publisherValue}`;
+      isFirstSearchField = false;
+    }
+  }
+  if (pltValue) {
+    if (!isFirstSearchField) {
+      url = url + `&plt<${pltValue}`;
+    } else {
+      url = url + `?plt<${pltValue}`;
+      isFirstSearchField = false;
+    }
+  }
+  if (pgtValue) {
+    if (!isFirstSearchField) {
+      url = url + `&pgt>${pltValue}`;
+    } else {
+      url = url + `?pgt>${pltValue}`;
+      isFirstSearchField = false;
+    }
+  }
+  return url;
 }
 
 window.onload = () => {
