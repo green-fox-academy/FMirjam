003539286ab95c49348ec6c1a29b4ed78e1f21d6
Mirjam <fmirka.fm@gmail.com>
2021-03-21 16:19:49 +0100

    syntax error solved

--- week-13/day 1-2/Import_user_data/index.js
+++ week-13/day 1-2/Import_user_data/index.js
@@ -2,6 +2,9 @@ import mysql from 'mysql';
 import fs from 'fs';
 import express from 'express';
 
+const app = express();
+const PORT = 3001;
+
 const databaseConnection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -10,20 +13,22 @@ const databaseConnection = mysql.createConnection({
 
 databaseConnection.connect((err) => {
   if (err) {
-    console.error('Cannot connect to the database', err);
-    return;
+    throw err;
   }
   console.log('Database Connection is OK');
 });
 
-try {
-  let csvContent = fs.readFileSync('users.csv', 'utf-8');
-  let splittedContent = csvContent.split('\n');
-  for (let i = 0; i < splittedContent.length; i++) {
-    let contentInLines = splittedContent[i].split(';');
-    if (i === 0) {
-      let columns = contentInLines;
-      let tableCreateQuery = `CREATE TABLE IF NOT EXISTS users (
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  try {
+    let csvContent = fs.readFileSync('users.csv', 'utf-8');
+    let splittedContent = csvContent.split('\n');
+    for (let i = 0; i < splittedContent.length; i++) {
+      let contentInLines = splittedContent[i].split(';');
+      if (i === 0) {
+        let columns = contentInLines;
+        let tableCreateQuery = `CREATE TABLE IF NOT EXISTS users (
         ${columns[0]}  INT NOT NULL AUTO_INCREMENT,
         ${columns[1]}  VARCHAR(45) NULL,
         ${columns[2]}  VARCHAR(45) NULL,
@@ -34,42 +39,47 @@ try {
         ${columns[7]}  VARCHAR(50) NULL,
         PRIMARY KEY ${columns[0]})`;
 
-      databaseConnection.query(tableCreateQuery, (err, rows) => {
-        if (err) {
-          console.log(err.message);
-          return;
-        }
-        console.log('Table created');
-      });
-    } else {
-      let values = {
-        id: contentInLines[0],
-        prefix: contentInLines[1],
-        first_name: contentInLines[2],
-        last_name: contentInLines[3],
-        address: contentInLines[4],
-        height: contentInLines[5],
-        bitcoin_address: contentInLines[6],
-        color_preference: contentInLines[7],
-      };
-      databaseConnection.query(
-        'INSERT INTO users SET ?',
-        values,
-        (err, rows) => {
+        databaseConnection.query(tableCreateQuery, (err, rows) => {
           if (err) {
             console.log(err.message);
             return;
           }
-          // console.log('Data added');
-        }
-      );
+          console.log('Table created');
+        });
+      } else {
+        let values = {
+          id: parseInt(contentInLines[0]),
+          prefix: contentInLines[1],
+          first_name: contentInLines[2],
+          last_name: contentInLines[3],
+          address: contentInLines[4],
+          height: contentInLines[5],
+          bitcoin_address: contentInLines[6],
+          color_preference: contentInLines[7],
+        };
+        databaseConnection.query(
+          'INSERT INTO users SET ?',
+          values,
+          (err, rows) => {
+            if (err) {
+              console.log(err.message);
+              return;
+            }
+            console.log('Data added');
+          }
+        );
+      }
     }
+  } catch (err) {
+    console.log(err.message);
   }
-} catch (err) {
-  console.log(err.message);
-}
+});
 
 process.on('uncaughtException', (err) => {
   console.log('Fatal error occured', err.message);
   process.exit(1);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
