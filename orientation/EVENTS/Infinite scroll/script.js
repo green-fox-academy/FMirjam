4e731284db22dcb718caa2399391d2bad838734f
Mirjam <fmirka.fm@gmail.com>
2021-03-25 21:37:19 +0100

    infinite scroll is WIP

--- orientation/EVENTS/Infinite scroll/script.js
+++ orientation/EVENTS/Infinite scroll/script.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 const randomNumberOfDivs = Math.floor(Math.random() * 10 + 5);
+let scrollThreshold = 300;
 
 function createDivs(numberOfDivs) {
   for (let i = 0; i < numberOfDivs; i++) {
@@ -14,7 +15,14 @@ createDivs(randomNumberOfDivs);
 
 window.addEventListener('scroll', () => {
   let existingDivs = document.querySelectorAll('div').length;
-  if (window.scrollY > existingDivs - window.innerHeight) {
+  if (window.scrollY > existingDivs - window.innerHeight - scrollThreshold) {
     createDivs(10);
+  }
+  if (window.scrollY < scrollThreshold) {
+    for (let i = 0; i < 10; i++) {
+      let div = document.createElement('div');
+      div.style.backgroundColor = '#' + randomColour;
+      body.insertBefore(div, bodyChildNodes[0]);
+    }
   }
 });
