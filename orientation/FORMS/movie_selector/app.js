ef82fc20a57877850b06a832b430684c6e5a22ef
Mirjam <fmirka.fm@gmail.com>
2021-04-07 12:56:26 +0200

    movie selector app.js is working but not perfectly

--- orientation/FORMS/movie_selector/app.js
+++ orientation/FORMS/movie_selector/app.js
@@ -1,7 +1,29 @@
-const genres = document.querySelector('#genre');
-const movies = document.querySelector('#movie');
+const genreSelector = document.querySelector('#genre');
+const movieSelector = document.querySelector('#movie');
 const result = document.querySelector('p');
 
-genres.addEventListener('change', () => {});
+const movies = {
+  'sci-fi': ['Moon', '2001: A Space Odyssey', 'Contact'],
+  drama: ['Darkest Hour', 'There Will Be Blood', 'American Beauty'],
+  comedy: ['Airplane!', 'Deadpool', 'Wayne World'],
+};
 
-movies.addEventListener('change', () => {});
+genreSelector.addEventListener('change', (event) => {
+  const selectAMovieOption = new Option();
+  movieSelector.add(selectAMovieOption);
+  let selectedIndex = event.target.selectedIndex;
+  let selectedGenre = event.target[selectedIndex].value;
+  if (movies[selectedGenre] !== undefined) {
+    movieSelector.innerHTML = '';
+    movies[selectedGenre].forEach((movie) => {
+      let optionElement = new Option(movie);
+      movieSelector.add(optionElement);
+    });
+  }
+});
+
+movieSelector.addEventListener('change', (event) => {
+  let selectedIndex2 = event.target.selectedIndex;
+  let selectedMovie = event.target[selectedIndex2].value;
+  result.innerHTML = `The selected movie is: ${selectedMovie}`;
+});
