f95f9e0ff7a951e2bae23e28f264b349a35fc13e
Mirjam <fmirka.fm@gmail.com>
2021-04-14 21:00:47 +0200

    error to be checked tomorrow

---
+++ orientation/BACKEND_TESTING/Groot/routes.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mysql from 'mysql';
+const app = express();
+
+app.use(express.json());
+
+const databaseConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'groot',
+});
+
+databaseConnection.connect((err) => {
+  if (err) {
+    console.error('Cannot connect to the database', err);
+    return;
+  }
+  console.log('Database Connection is OK');
+});
+
+app.get('/groot', (req, res) => {
+  let message = req.query.message;
+  let response = {};
+  if (message) {
+    response = { received: 'somemessage', translated: 'I am Groot!' };
+  } else {
+    response = { error: 'I am Groot!' };
+  }
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('Fatal error occured', err.message);
+  process.exit(1);
+});
+
+module.exports = app;
---
+++ orientation/BACKEND_TESTING/Groot/test/test-routes.js
@@ -0,0 +1,4 @@
+import test from 'tape'
+import request from 'supertest'
+import expect from 'chai'
+// const app = require('../routes');
