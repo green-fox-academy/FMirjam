1c7046959ccd7d7df5f7fbdfcf75a2368aa9bb95
Mirjam <fmirka.fm@gmail.com>
2021-04-15 21:36:09 +0200

    cargo endpoints done

--- orientation/BACKEND_TESTING/Cargo/server.js
+++ orientation/BACKEND_TESTING/Cargo/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mysql from 'mysql';
 export const app = express();
-const port = 3001;
+const port = 3002;
 
 app.use(express.json());
 
@@ -18,13 +18,54 @@ databaseConnection.connect((err) => {
   console.log('Database Connection is OK');
 });
 
-app.get('/rocket', (req,res) => {
+app.get('/rocket', (req, res) => {
+  res.status(200).send({
+    caliber25: 0,
+    caliber30: 0,
+    caliber50: 0,
+    shipstatus: 'empty',
+    ready: false,
+  });
+});
 
-})
+app.get('/rocket/fill', (req, res) => {
+  let caliber = req.query.caliber;
+  let amount = req.query.amount;
+  let response = {};
+  let maximumAmount = 12500;
 
-app.get('/rocket/fill', (req,res) => {
-
-})
+  function shipStatus(amount, maximumAmount) {
+    let ratio = amount / maximumAmount;
+    let ratioInPercentage = ratio * 100;
+    if (ratio === 0) {
+      return 'empty';
+    } else if (ratio === 1) {
+      return 'full';
+    } else if (ratio > 1) {
+      return 'overloaded';
+    } else {
+      return `${ratioInPercentage}%`;
+    }
+  }
+  function ready(amount, maximumAmount) {
+    let ratio = amount / maximumAmount;
+    if (ratio === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (caliber && amount) {
+    response = {
+      received: `${caliber}`,
+      amount: `${amount}`,
+      shipstatus: shipStatus(amount, maximumAmount),
+      ready: ready(amount, maximumAmount),
+    };
+    res.status(200).send(response);
+    console.log(response);
+  }
+});
 
 process.on('uncaughtException', (err) => {
   console.log('Fatal error occured', err.message);
