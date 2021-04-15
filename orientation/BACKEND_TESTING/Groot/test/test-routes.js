f8884c0027f6a3f2e3ed656d1a066dfa35440999
Mirjam <fmirka.fm@gmail.com>
2021-04-15 14:19:13 +0200

    test with message done

--- orientation/BACKEND_TESTING/Groot/test/test-routes.js
+++ orientation/BACKEND_TESTING/Groot/test/test-routes.js
@@ -1,5 +1,24 @@
-import request from 'supertest'
-import chai from 'chai'
-import {app} from '../routes.js'
-let expect = chai.expect
+import request from 'supertest';
+import chai from 'chai';
+import { app } from '../routes.js';
 
+let expect = chai.expect;
+
+describe('groot endpoint with message', () => {
+  it('should return a message', (done) => {
+    const message = 'test';
+    request(app)
+      .get(`/groot?message=${message}`)
+      .expect(200)
+      .end((err, res) => {
+        expect(err).to.be.null;
+
+        expect(res.body).to.deep.equal({ //use deep equal when we compare objects
+          received: `${message}`,
+          translated: 'I am Groot!',
+        });
+
+        done();
+      });
+  });
+});
