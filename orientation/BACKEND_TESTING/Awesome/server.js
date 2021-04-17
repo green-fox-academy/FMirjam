3173bf7ec92f36d329b07d0547e8d6f41aaa6a5f
Mirjam <fmirka.fm@gmail.com>
2021-04-17 11:10:40 +0200

    post endpoint done

--- orientation/BACKEND_TESTING/Awesome/server.js
+++ orientation/BACKEND_TESTING/Awesome/server.js
@@ -20,24 +20,59 @@ databaseConnection.connect((err) => {
 });
 
 app.get('/awesome', (req, res) => {
-    databaseConnection.query('SELECT * FROM playlist', (err, rows) => {
-      if (err) {
+  databaseConnection.query('SELECT * FROM playlist', (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+      });
+      return;
+    } else {
+      if (rows.length === 0) {
         res.status(500).json({
           error: err.message,
         });
         return;
       } else {
-        if (rows.length === 0) {
-          res.status(500).json({
-            error: err.message,
-          });
-          return;
-        } else {
-          res.status(200).json(rows);
-        }
+        res.status(200).json(rows);
       }
-    });
+    }
   });
+});
+
+app.post('/awesome', (req, res) => {
+  databaseConnection.query('SELECT * FROM playlist', (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+      });
+      return;
+    } else if (rows.length !== 0) {
+      let newPlaylistItem = {
+        author_name: req.body.author,
+        title: req.body.title,
+        genre: req.body.genre,
+        year: parseInt(req.body.year),
+        rating: parseInt(req.body.rating),
+      };
+      databaseConnection.query(
+        'INSERT INTO playlist SET ?',
+        [newPlaylistItem],
+        (err, rows) => {
+          if (err) {
+            res.status(500).json({
+              error: err.message,
+            });
+            return;
+          } else {
+            res.status(200).json({ result: 'New song has been registered' });
+          }
+        }
+      );
+    } else {
+      res.status(400).send();
+    }
+  });
+});
 
 process.on('uncaughtException', (err) => {
   console.log('Fatal error occured', err.message);
