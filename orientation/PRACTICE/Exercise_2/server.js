90dd98e4cf44ee452a5bb545f0be4e8bd557ad90
Mirjam <fmirka.fm@gmail.com>
2021-05-04 09:08:17 +0200

    start backend

---
+++ orientation/PRACTICE/Exercise_2/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+const PORT = 3010;
+
+const databaseConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'exercise2',
+});
+
+databaseConnection.connect((err) => {
+    if (err) {
+      console.error('Cannot connect to the database', err);
+      return;
+    }
+    console.log('Database Connection is OK');
+  });
+  app.use(cors());
+  app.use(express.static('public'));
+  app.use(express.json());
+
+  process.on('uncaughtException', (err) => {
+    console.log('Fatal error occured', err.message);
+    process.exit(1);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
