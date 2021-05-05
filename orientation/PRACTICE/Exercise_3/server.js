4dbd3593689855543712b95e735a11f4b900d75f
Mirjam <fmirka.fm@gmail.com>
2021-05-05 20:00:40 +0200

    render html done

---
+++ orientation/PRACTICE/Exercise_3/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+const PORT = 3011;
+
+const databaseConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'ratemymentor',
+});
+
+databaseConnection.connect((err) => {
+  if (err) {
+    console.error('Cannot connect to the database', err);
+    return;
+  }
+  console.log('Database Connection is OK');
+});
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(path.resolve() + 'public/index.html'));
+});
+
+
+process.on('uncaughtException', (err) => {
+    console.log('Fatal error occured', err.message);
+    process.exit(1);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
