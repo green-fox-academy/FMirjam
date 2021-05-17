b3683c95279424695e09bd4a6841ec1597ea93bd
Mirjam <fmirka.fm@gmail.com>
2021-05-17 19:49:00 +0200

    first try out with a manually created data

---
+++ project phase/weather-app/src/db-data.ts
@@ -0,0 +1,21 @@
+export const TILES: any = [
+  {
+    id: 1,
+    city: 'Seattle',
+    state: 'Washington',
+    degree: 11,
+  },
+  {
+    id: 2,
+    city: 'Miami',
+    state: 'Florida',
+    degree: 33,
+  },
+
+  {
+    id: 3,
+    city: 'Barcelona',
+    state: 'Spain',
+    degree: 19,
+  },
+];
