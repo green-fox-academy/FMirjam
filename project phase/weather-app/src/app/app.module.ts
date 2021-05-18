a2a71349a2c80daf49fe104d0da2959d253dc00c
Mirjam <fmirka.fm@gmail.com>
2021-05-18 20:21:20 +0200

    import HttpíClientModule to app module

--- project phase/weather-app/src/app/app.module.ts
+++ project phase/weather-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { WeatherService } from 'src/weather.service';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { TileComponent } from './tile/tile.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [WeatherService],
