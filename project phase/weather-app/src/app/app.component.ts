e79bc83c0bf65a26f4988ba66a392096166f1bce
Mirjam <fmirka.fm@gmail.com>
2021-05-28 15:07:05 +0200

    added scss to dashboard component

--- project phase/weather-app/src/app/app.component.ts
+++ project phase/weather-app/src/app/app.component.ts
@@ -8,21 +8,5 @@ import { ITile } from './model/Itile';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  // title = 'weather-app';
-  // tiles: ITile[];
-  // cityName: string = '';
-
-  // constructor(private service: WeatherService) {
-  //   this.service.printToConsole('Got the service');
-  // }
-
-  ngOnInit(): void {
-    // this.service.getApiData().subscribe((response) => (this.tiles = response));
-  }
-
-  // show() {
-  //   this.service.getApiDataByCityName(this.cityName).subscribe((response) => {
-  //     this.tiles.push(response);
-  //   });
-  // }
+  ngOnInit(): void {}
 }
