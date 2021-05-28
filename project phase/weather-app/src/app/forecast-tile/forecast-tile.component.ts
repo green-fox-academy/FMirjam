9c148bcb0981ccf7021c2732647252ba30180635
Mirjam <fmirka.fm@gmail.com>
2021-05-28 17:21:48 +0200

    forecast wip

--- project phase/weather-app/src/app/forecast-tile/forecast-tile.component.ts
+++ project phase/weather-app/src/app/forecast-tile/forecast-tile.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherService } from 'src/weather.service';
+import { IForecastTile } from '../model/IForecastTile';
 
 @Component({
   selector: 'app-forecast-tile',
   templateUrl: './forecast-tile.component.html',
-  styleUrls: ['./forecast-tile.component.scss']
+  styleUrls: ['./forecast-tile.component.scss'],
 })
 export class ForecastTileComponent implements OnInit {
+  @Input()
+  forecastTile: IForecastTile;
 
-  constructor() { }
+  constructor(private service: WeatherService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
