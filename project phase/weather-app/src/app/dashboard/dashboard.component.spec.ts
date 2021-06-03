71576f7906a7c5f05fa870cf03f3926b42e8894b
Mirjam <fmirka.fm@gmail.com>
2021-06-03 13:35:13 +0200

    test added

--- project phase/weather-app/src/app/dashboard/dashboard.component.spec.ts
+++ project phase/weather-app/src/app/dashboard/dashboard.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
-import {HttpClientTestingModule} from '@angular/common/http/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { WeatherService } from 'src/weather.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let weatherServiceSpy: jasmine.SpyObj<WeatherService>;
 
   beforeEach(async () => {
+    weatherServiceSpy = jasmine.createSpyObj<WeatherService>('service', [
+      'getApiData',
+      'getApiDataByCityName',
+    ]);
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
+      declarations: [DashboardComponent],
+      providers: [
+        {
+          provide: WeatherService,
+          useValue: weatherServiceSpy,
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -25,4 +36,18 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should call service getApiDataByCityName with input cityName', () => {
+  //   // Arrange
+  //   component.cityName = 'Budapest'
+  //   weatherServiceSpy.getApiDataByCityName.and.returnValue(of(testTileData));
+
+  //   // Act
+  //   component.show()
+
+  //   // Assert
+  //   expect(weatherServiceSpy.getApiDataByCityName).toHaveBeenCalledOnceWith(
+  //     'Budapest'
+  //   );
+  // });
 });
--- project phase/weather-app/src/app/tile/tile.component.spec.ts
+++ project phase/weather-app/src/app/tile/tile.component.spec.ts
@@ -1,10 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ITile } from '../model/Itile';
 
 import { TileComponent } from './tile.component';
 
 describe('TileComponent', () => {
   let component: TileComponent;
   let fixture: ComponentFixture<TileComponent>;
+
+  let testTileData: ITile = {
+    id: 1,
+    city: 'Budapest',
+    state: 'HU',
+    degree: 25,
+    image: 'testIcon',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,5 +29,17 @@ describe('TileComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit id = 1 when call showForecastData()', () => {
+    // Arrange
+    component.tile= testTileData;
+    spyOn(component.getForecast, 'emit');
+
+    // Act
+    component.showForecastData();
+
+    // Assert
+    expect(component.getForecast.emit).toHaveBeenCalledOnceWith(1);
   });
 });
